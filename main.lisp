7ac16845092d5f7769d5c37b1e7aa35bb2f9d74d
Nisen <imnisen@gmail.com>
2018-12-06 08:33:34 +0800

    Add multi value do* macro

--- main.lisp
+++ main.lisp
@@ -104,3 +104,53 @@
                               `(append (nthcdr ,n ,src) (subseq ,src 0 ,n)))
                             (1- (length parms))))))))
 
+;;; Chapter 11.5
+(defmacro mvdo* (parm-cl test-cl &body body)
+  (mvdo-gen parm-cl parm-cl test-cl body))
+
+(defun mvdo-gen (binds rebinds test body)
+  (if (null binds)
+      (let ((label (gensym)))
+        `(prog nil
+            ,label
+            (if ,(car test)
+                (return (progn ,@(cdr test))))
+            ,@body
+            ,@(mvdo-rebind-gen rebinds)
+            (go ,label)))
+      (let ((rec (mvdo-gen (cdr binds) rebinds test body)))
+        (let ((var/s (caar binds)) (expr (cadar binds))) (if (atom var/s)
+                                                             `(let ((,var/s ,expr)) ,rec) `(multiple-value-bind ,var/s ,expr ,rec))))))
+(defun mvdo-rebind-gen (rebinds)
+  (cond ((null rebinds) nil)
+        ((< (length (car rebinds)) 3)
+         (mvdo-rebind-gen (cdr rebinds)))
+        (t
+         (cons (list (if (atom (caar rebinds))
+                         'setq
+                         'multiple-value-setq)
+                     (caar rebinds)
+                     (third (car rebinds)))
+               (mvdo-rebind-gen (cdr rebinds))))))
+
+
+;;; example
+;; (mvdo* ((x 1 (1+ x))
+;;         ((y z) (values 0 0) (values z x)))
+;;     ((> x 5) (list x y z))
+;;   (princ (list x y z)))
+
+;; Macro expand to:
+
+;; (LET ((X 1))
+;;   (MULTIPLE-VALUE-BIND (Y Z)
+;;       (VALUES 0 0)
+;;     (PROG ()
+;;        #:G593
+;;        (IF (> X 5)
+;;            (RETURN (PROGN (LIST X Y Z))))
+;;        (PRINC (LIST X Y Z))
+;;        (SETQ X (1+ X))
+;;        (MULTIPLE-VALUE-SETQ (Y Z) (VALUES Z X))
+;;        (GO #:G593))))
+
